82f38cbf789ea0788c70c429c7fdbbb871f9a90e
Kittipitch_Sertsorn <kittipitch.mold@mail.kmutt.ac.th>
2023-03-08 16:34:23 +0700

    working on unity project

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -13,18 +13,58 @@ public class Fractal : MonoBehaviour
     [SerializeField]
     private float size;
 
+    List<GameObject> cubes = new List<GameObject>();
+
     void Start()
     {
         //  This statement create a game object given a prefab, position and rotation.
-        Instantiate(
-            //  This parameter sets a prefab for an instance to be created.
-            cubePrefab,
+        GameObject cube =  Instantiate(cubePrefab, Vector3.zero, Quaternion.identity);
+        cube.transform.localScale = new Vector3(size, size, size);
+        cubes.Add(cube);
+    }
 
-            //  This parameter sets an instance position to (0, 0, 0).
-            Vector3.zero,
+    void Update()
+    {
+        if (iteration > 1)
+        {
+            List<GameObject> newCubes = Split(cubes);
 
-            //  This parameter sets an instance rotation to (0, 0, 0).
-            Quaternion.identity
-        );
+            foreach (var cube in cubes)
+            {
+                Destroy(cube);
+            }
+
+            cubes = newCubes;
+        }
+    }
+
+    List<GameObject> Split(List<GameObject> cubes)
+    {
+        List<GameObject> subCubes = new List<GameObject>();
+
+        foreach(var cube in cubes)
+        {
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    for (int z = 0; z < 3; z++)
+                    {
+                        float xx = x * (size);
+                        float yy = y * (size);
+                        float zz = z * (size);
+
+                        Vector3 cubePos = new Vector3(xx, yy, zz) + cube.transform.position;
+                        
+                        GameObject copy = Instantiate(cube, cubePos, Quaternion.identity);
+                        copy.transform.localScale = new Vector3(size, size, size);
+                    }
+                }
+            }
+        }
+
+
+        return subCubes;
     }
 }
